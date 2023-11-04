54e749084007aab9b5d23fcc99d9dbcb52602de1
Robert Nastasa <nrarobert@gmail.com>
2023-11-04 14:03:30 +0200

    Added functionality for 'Add new question' button + some visual changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
-import TsCompTest from './components/tsCompTest/tsCompTest.tsx';
+import { useState } from 'react';
 
 function App() {
+  const [showAddQuestion, setShowAddQuestion] = useState(false);
+  const [getInputText, setGetInputText] = useState('');
+  const [getDoneQuestion, setGetDoneQuestion] = useState([]);
+
+  const handleAddQuestion = () => {
+    setShowAddQuestion(true);
+  }
+
+  const handleInputText = (e) => {
+    setGetInputText(e.target.value);
+  }
+
+  const handleDoneQuestion = (e) => {
+    e.preventDefault();
+    setGetDoneQuestion([...getDoneQuestion, getInputText]);
+    setGetInputText('');
+  }
+
   return (
     <div className="App">
-      <TsCompTest title='fasung' text='confused unga bunga' />
+
+      {
+        getDoneQuestion.map((question, index) => {
+          return (
+            <div key={index + 1}>
+              <span>{index + 1}</span>
+              <h4>{question}</h4>
+            </div>
+          )
+        })
+      }
+
+
+
+      {
+        showAddQuestion ? 
+        <form>
+          <input value={getInputText} onChange={handleInputText} type='text'></input>
+          <button onClick={handleDoneQuestion}>Done</button>
+        </form> :
+        <></>
+      }
+      
+      <button onClick={handleAddQuestion}>Add new question</button>
     </div>
   );
 }
